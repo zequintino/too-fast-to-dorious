3c1fb2103b0d7fe8ea671304751fb570770c62b1
zequintino <108005850+zequintino@users.noreply.github.com>
2025-03-31 02:54:25 +0100

    renamed brian checks page

---
+++ src/pages/ChecklistPage.tsx
@@ -0,0 +1,9 @@
+import CheckupList from "../components/CheckupList/CheckupList";
+
+export default function ChecklistPage() {
+  return (
+    <div className="page checkup-page">
+      <CheckupList />
+    </div>
+  );
+}
